2f7cdf94ab20e7d31b963fe59bd973a8d9725bb6
Latrice Nwatu <lathomas4@student.fullsail.edu>
2026-07-03 22:31:58 -0500

    initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const { headers, url, method } = req;
+  console.log(headers, url, method);
+  res.end();
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
