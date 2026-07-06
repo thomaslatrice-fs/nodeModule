ae76ee30b63a50e3156e0a652ffbbcf55e6f853d
Latrice Nwatu <lathomas4@student.fullsail.edu>
2026-07-05 21:55:25 -0500

    NodeJS Coding assignment complete

---
+++ app/routes/index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+//localhost: 3000/
+
+let users = [
+  {
+    id: 601,
+    data: "This is a message!",
+  },
+
+  {
+    id: 901,
+    data: "This is a message!",
+  },
+
+  {
+    id: 612,
+    data: "This is a message!",
+  },
+];
+
+//actuator/root route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Service is up",
+    metadata: {
+      hostname: "localhost",
+      method: "GET",
+    },
+  });
+});
+
+//GET all
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+//GET by ID
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json(user);
+});
+
+//POST by ID
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: req.body.id,
+    name: req.body.name,
+  };
+  users.push(newUser);
+  res.status(201).json({
+    messgae: "User added",
+    user: newUser,
+    users: users,
+  });
+});
+
+//PUT by ID
+app.put("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  users[userIndex] = {
+    id: id,
+    name: req.body.name,
+  };
+  res.status(200).json({
+    message: "User updated",
+    user: usrers[userIndex],
+    users: users,
+  });
+});
+
+//DELETE by ID
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const deleteUser = users.splice(userIndex, 1);
+
+  res.status(200).json({
+    message: "User deleted",
+    deleteduser: edeletedUser[0],
+    users: users,
+  });
+});
+
+module.exports = app;
